298587644fa4cb13c456637106351c8f4ec80b9c
Tim Martinez <83369306+timperry151@users.noreply.github.com>
2023-06-24 13:14:43 -0400

    add the ability for user to generate a custom grid size between 1-100.

--- main.js
+++ main.js
@@ -1,17 +1,51 @@
+
+//declare variable for container
 const container = document.querySelector('.container');
 
-for(let i = 0; i < 256; i++){
-    const div = document.createElement('div');
-    div.classList.add('pixel');
-    div.style.cssText = 'box-sizing: border-box; background-color: black; height: 50px; width: 50px; border: 1px solid gray; padding: 0';
-    
-    container.appendChild(div);
+//generate grid within 960px
+drawGrid(16)
+
+function drawGrid(input){
+    for(let i = 0; i < (input * input); i++){
+        const div = document.createElement('div');
+        div.classList.add('pixel');
+        div.style.cssText = 'box-sizing: border-box; background-color: black; border: 1px solid gray; padding: 0';
+        div.style.height = div.style.width = 960 / input + 'px';
+        container.appendChild(div);
+    };
 };
 
-const pixel = document.querySelectorAll('.pixel');
+//"pixel" hover effect
+pixelEffect();
 
-pixel.forEach(pixel => {
-    pixel.addEventListener('mouseover', () => {
-        pixel.style.backgroundColor = 'green';
+function pixelEffect(){  
+    const pixel = document.querySelectorAll('.pixel');
+
+    pixel.forEach(pixel => {
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = 'green';
+        });
     });
+};
+
+//click button prompt user and generate new grid
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    
+    //prompt user 
+    let input = prompt('Enter a new grid size up to 100 pixels.');
+    //limit input to 1-100 
+    while (input > 100 || input < 1){
+        input = prompt('Enter a new grid size up to 100 pixels.')
+    };
+
+    //remove old divs
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    };
+
+    drawGrid(input);
+
+    pixelEffect();
+
 });
